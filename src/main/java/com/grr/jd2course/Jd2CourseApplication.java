d636ce2550bf0940b2891ab69709adc4e48f5215
rashidgafurov <nsd7c4@gmail.com>
2022-07-04 11:57:30 -0400

    Initial commit

---
+++ src/main/java/com/grr/jd2course/Jd2CourseApplication.java
@@ -0,0 +1,13 @@
+package com.grr.jd2course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jd2CourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jd2CourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grr/jd2course/Jd2CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grr.jd2course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jd2CourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
